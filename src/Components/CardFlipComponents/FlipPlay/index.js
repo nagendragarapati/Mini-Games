49ed086a3673808c8f45073ffcdc67173e9bd884
garapati.nagendra <garapati.nagendra@infosys.com>
2024-02-07 20:23:37 +0530

    flip-game-added

--- src/Components/CardFlipComponents/FlipPlay/index.js
+++ src/Components/CardFlipComponents/FlipPlay/index.js
@@ -1,4 +1,10 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { flipRulesSet1, flipRulesSet2 } from "../../GlobalRules"
+import RulesModal from "../../GlobalRulesModal"
 
+
+import "./index.css"
 
 const imgs_array = [
     {
@@ -32,27 +38,31 @@ const imgs_array = [
     }, {
         id: 10,
         url: "/Images/Group 7484hippo.png"
-    }, {
-        id: 11,
-        url: "/Images/Group 7485pegion.png"
-    }, {
-        id: 12,
-        url: "/Images/Layer 2daino.png"
-    }, {
-        id: 13,
-        url: "/Images/Layer 2elephant.png"
-    }, {
-        id: 14,
-        url: "/Images/Layer 2panda.png"
-    }, {
-        id: 15,
-        url: "/Images/Layer 2tiger.png"
-    }, {
-        id: 16,
-        url: "/Images/Layer 3zeebra.png"
-    }
+    }, 
+    // {
+    //     id: 11,
+    //     url: "/Images/Group 7485pegion.png"
+    // }, {
+    //     id: 12,
+    //     url: "/Images/Layer 2daino.png"
+    // }, {
+    //     id: 13,
+    //     url: "/Images/Layer 2elephant.png"
+    // }, {
+    //     id: 14,
+    //     url: "/Images/Layer 2panda.png"
+    // }, {
+    //     id: 15,
+    //     url: "/Images/Layer 2tiger.png"
+    // }, {
+    //     id: 16,
+    //     url: "/Images/Layer 3zeebra.png"
+    // }
 ]
 const FlipPlay = () => {
+    const navigate = useNavigate();
+    const [modalShow, setModalShow] = useState(false);
+
 
     let doubel_imgs_array = imgs_array.concat(imgs_array)
     let shuffled_array = []
@@ -62,21 +72,52 @@ const FlipPlay = () => {
         shuffled_array.push(doubel_imgs_array[randomNum])
         doubel_imgs_array.splice(randomNum, 1);
     }
-    console.log(shuffled_array)
-
-
-
 
 
     return (
         <div className="flip-play-main">
+            <div className="flip-middle-container">
 
-            <h1>Flip Game</h1>
-            {
-                    shuffled_array.map(each => <img src={each.url} alt="imgs" className="icon-image"/>)
+                <div className="back-rule-header back-rule-header-flip">
+                    <div className="back-container" onClick={() => navigate(-1)}>
+                        <img src="/Images/arrow-leftarrowback.png" alt="arrow-leftarrowback" className="back-arrow" />
+                        <p className="back-text">Back</p>
+                    </div>
 
-            }
+                    <button type="button" className="rules-btn" onClick={() => setModalShow(true)}>Rules</button>
 
+                    <RulesModal
+                        show={modalShow}
+                        onHide={() => setModalShow(false)}
+                        rulesset1={flipRulesSet1} rulesset2={flipRulesSet2}
+                    />
+
+                </div>
+
+                <div className='flip-title-container'>
+                    <h1 className='flip-main-heading'>Card-Flip Memory Game</h1>
+
+                    <div className='score-timer-container'>
+                        <p className='flip-count game-result-values'>Card flip count - 00</p>
+                        <p className='time-flip game-result-values'>02:00</p>
+                        <p className='score-sec game-result-values'>Score - 00</p>
+                    </div>
+                </div>
+
+                <ul className='flip-cards-container'>
+                    {
+                        shuffled_array?.map(card => {
+                            return (
+                                <li className='flip-image-container'>
+                                    <img src={card.url} alt='flip-img' className='flip-image' />
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+
+
+            </div>
         </div>
 
 
